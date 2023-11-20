49c5b3e7caa6d820b99cc381503a45666496058a
Brian Nguyen <briannguyen636@gnail.com>
2023-11-19 22:01:40 -0800

    bootswatch implementation

--- dashers-app/src/App.js
+++ dashers-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+
 import OrderSum from './pages/OrderSum';
 
 function App() {
--- dashers-app/src/pages/OrderSum.js
+++ dashers-app/src/pages/OrderSum.js
@@ -16,30 +16,42 @@ function OrderSum() {
     );
 
     return (
-        <div id = "orderSum"> 
-            <h1>Order Summary</h1>
-            <hr/>
-            <div id = "orderBody">
-                <ul id = "itemsOrdered">
-                    <li>{items[0].name}</li>
-                    <ul>
-                        <li>{items[0].price}</li>
+        <div> 
+            <div class = "navbar bg-dark">
+                <h1 style={{color:"white", paddingLeft:"20px"}}> <button class = "btn btn-secondary">...</button>  Dashers</h1>
+            </div>
+            <br/><br/>
+            <div class = "card" id = "orderSum">
+                <br/>
+                <h1>Order Summary</h1>
+                <hr/>
+                <div id = "orderBody">
+                    <ul id = "itemsOrdered">
+                        <li>{items[0].name}</li>
+                        <ul>
+                            <li>{items[0].price}</li>
+                        </ul>
                     </ul>
-                </ul>
-                <p>Order Total: {orderTotal}</p>
+                    <p>Order Total: {orderTotal}</p>
+                    <button class = "btn btn-primary">Edit Order</button>
+                </div>
+                <hr/>
+                <h1>Order Details </h1>
+                <hr/>
+                <div id = "orderDetails">
+                    <ul id = "detailList">
+                        <li>Customer Name: {customer.name}</li>
+                        <li>Delivery Address: {customer.address}</li>
+                        <li>Payment: {}</li>
+                        <li>Test:</li>
+                    </ul>
+                    <button class = "btn btn-primary">Edit Details</button>
+                </div>
+                <hr/>
+            
+                <button type = "button" class = "btn btn-primary"> Place Order </button>
+                <br/>
             </div>
-            <hr/>
-            <h1>Order Details</h1>
-            <hr/>
-            <div id = "orderDetails">
-                <ul id = "detailList">
-                    <li>Customer Name: {customer.name}</li>
-                    <li>Delivery Address: {customer.address}</li>
-                    <li>Payment: {}</li>
-                    <li>Test:</li>
-                </ul>
-            </div>
-            <button type = "button"> Place Order </button>
         </div>
     );
 };
