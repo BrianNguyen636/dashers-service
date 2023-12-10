20cfc45cfc3544ecf7d835d8d6a518fe205c93a3
Amrit Paul Singh <amrit9928@gmail.com>
2023-12-09 22:19:08 -0800

    updated menu page

--- dashers-app/src/App.js
+++ dashers-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; 
 import RestaurantPage from "./pages/RestaurantPage";
 import OrderSum from './pages/OrderSum';
 import RestaurantDetail from "./pages/RestaurantDetail";
@@ -9,6 +9,9 @@ return (
 <div className="app"> 
 <BrowserRouter> 
 <Routes> 
+  <Route path="/"element={<Navigate to="/home" replace />}/>
+  {/* Temporary, need to replace /home link to point towards actual home page, 
+      probs just point to user registration page like we discussed */}
   <Route path="/home" element={<RestaurantPage />} />
   <Route path="/res" element={<RestaurantPage />} />
   <Route path="/restaurant/detail/:RestaurantID" element={<RestaurantDetail />} />
